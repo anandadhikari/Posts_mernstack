e84c007013a5adf29106fa7eee97c765f7a1735d
anandadhikari <anandadhikari02@gmail.com>
2021-08-21 13:30:35 +0530

    a

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 //Import routes
-const posts = require('./routes/post');
-const users = require('./routes/user');
+const posts = require("./routes/post");
+const users = require("./routes/user");
 
 dotenv.config();
 
@@ -15,11 +15,11 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-} else if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else if (process.env.NODE_ENV === "production") {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
   });
 }
 
@@ -27,10 +27,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', posts);
-app.use('/api', users);
+app.use("/api", posts);
+app.use("/api", users);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
