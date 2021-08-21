f900ee73840c912e146528283bdf847a90b27cf8
anandadhikari <anandadhikari02@gmail.com>
2021-08-21 15:05:04 +0530

    s'

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use("/api", posts);
 app.use("/api", users);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
