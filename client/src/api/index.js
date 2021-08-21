83091a1ec8281f0ab98e3f441483b4ecec1ab001
anandadhikari <anandadhikari02@gmail.com>
2021-08-21 14:10:40 +0530

    as

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `https://posts-mern-stacks.herokuapp.com/`, // http://localhost:5000
 });
 
 API.interceptors.request.use((req) => {
